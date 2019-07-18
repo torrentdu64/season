9525c6824b852b2da0d91ea9be513de23687fa45
torrent benoit <torrentdu64@gmail.com>
2019-07-19 10:23:59 +1200

    class app geolocation done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+
+      (position) => {
+        this.setState({ lat: position.coords.latitude})
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message})
+      }
+    );
+  }
+
+  render(){
+
+    if( this.state.errorMessage && !this.state.lat ) {
+      return <div>message => {this.this.state.errorMessage }</div>;
+    }
+
+    if(!this.state.errorMessage && this.state.lat){
+      return <div>latitude {this.state.lat}</div>;
+    }
+
+    return <div>Loading ...</div>;
+
+  }
+
+}
+
+ReactDOM.render( <App />, document.querySelector('#root'))
